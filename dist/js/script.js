fdb53186d7965aedfb0d7319d36feacd6ba711c5
Marco Antonio <marcoantoniomadgaskar@gmail.com>
2024-02-03 01:26:47 +0700

    set animation and dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -33,3 +33,29 @@ window.addEventListener('click', function (e) {
 
 const currentYear = new Date().getFullYear();
 document.getElementById('Copyright').innerHTML = `&copy; Copyright ${currentYear}`;
+
+// Darkmode toggle
+$('#lightMode').on('click', function(){
+  $(this).find('i').toggleClass('bi-moon-stars-fill bi-cloud-sun-fill');
+  $('body').toggleClass('dark');
+  if ($('body').hasClass('dark')) {
+    localStorage.setItem('theme', 'dark');
+  } else {
+    localStorage.setItem('theme', 'light');
+    $('#lightMode').find('i').addClass('bi-cloud-sun-fill').removeClass('bi-moon-stars-fill');
+  }
+});
+
+$(document).ready(function(){
+  if (!localStorage.getItem('theme')) {
+    localStorage.setItem('theme', 'dark');
+  }
+
+  if(localStorage.theme === 'dark'){
+    $('#lightMode').find('i').addClass('bi-moon-stars-fill');
+    $('body').addClass('dark');
+  } else if(localStorage.theme === 'light') {
+    $('#lightMode').find('i').addClass('bi-cloud-sun-fill');
+    $('body').removeClass('dark');
+  }
+});
